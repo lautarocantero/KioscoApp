393c2ed65c97ea45187d782c52cfe8bdf719c3e8
lautaro <lautaroncantero@gmail.com>
2026-01-05 18:50:27 -0300

    added payment selection functionality

--- src/typings/sells/sells.ts
+++ src/typings/sells/sells.ts
@@ -1,6 +1,13 @@
+//─────────────────────────────── Payment Methods ───────────────────────────────//
 
-export enum PaymentMethods {
-  cash = 'efectivo',
-  card = 'tarjeta',
-  transfer = 'transferencia',
+export enum PaymentMethod {
+  Transfer = 'Transferencia',
+  Cash = 'Efectivo',
+  Debit = 'Débito',
+  Credit = 'Crédito'
+}
+
+export enum Currency {
+  Ars = 'ars',
+  Usd = 'usd',
 }
